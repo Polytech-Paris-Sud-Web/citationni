21a93e375a66385f7a7cc55022e504248c7e489d
renanAlibert <alibert.renan@gmail.com>
2021-04-12 12:33:32 +0200

    Revert "v1"
    This reverts commit 5fd2e0a2bb8166f04302737cceac74f7c0cc7ba8.

--- src/app/pipe/research.pipe.ts
+++ src/app/pipe/research.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ResearchPipe implements PipeTransform {
 
-  transform(items: any[], searchText: string): any[] {
+  transform(items: any[], searchText: string, type: any): any[] {
     if(!items) {
       return []
     }
